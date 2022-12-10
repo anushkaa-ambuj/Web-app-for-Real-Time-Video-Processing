60a925d8ef58573acafa37d86ca62836720793c0
PJiyush <jhamnani.1@iitj.ac.in>
2022-12-10 13:40:43 +0530

    changing real time video background implemented

--- Piyush/streaming.js
+++ Piyush/streaming.js
@@ -1,14 +1,55 @@
-console.log('Is it Working');
+// console.log('Is it Working');
 
 let video = document.getElementById("myVidPlayer")
 let canvas = document.getElementById("outputCanvas")
 let canvasContext = canvas.getContext('2d');
 
 
-console.log('Till now')
+// console.log('Till now')
 
 // streaming of video
 
 navigator.mediaDevices.getUserMedia({
     video:true
 }).then((value)=>video.srcObject=value).catch((err)=>console.log(err))
+
+
+// processing the video
+video.addEventListener('loadeddata',()=>{
+    // we are keeping the same dimension of video and canvas
+    let videoXPixels = video.videoWidth;
+    let videoYPixels = video.videoHeight;
+    canvas.width = videoXPixels;
+    canvas.height = videoYPixels;
+    // console.log(canvas.width, canvas.height)
+    
+    setInterval(() => {
+    // here video is coming as a single frame
+    canvasContext.drawImage(video, 0, 0, videoXPixels,videoYPixels);
+    // console.log('Yep it is working')
+    const videoFrameData = canvasContext.getImageData(0,0, canvas.width, canvas.height);
+    // console.log(videoFrameData) --> it is an object
+    // to access the image pixel data
+    let pixelLength = videoFrameData.data.length/4
+
+    for (let pixel = 0; pixel < pixelLength; pixel++) {
+        let interval = pixel*4
+        let r  = videoFrameData.data[interval+0]
+        let g  = videoFrameData.data[interval+1]
+        let b  = videoFrameData.data[interval+2]
+        let o  = videoFrameData.data[interval+3]
+        // console.log(r,g,b,o)
+        // condition to detect green background
+        if (g>117) {
+            if (g>b && g>r) {
+                videoFrameData.data[interval+3] = 0
+                console.log('is this block Working')
+            }
+        }
+        // console.log('checking the Processing');
+        
+        
+    }
+    canvasContext.putImageData(videoFrameData, 0,0);
+    },15);
+})
